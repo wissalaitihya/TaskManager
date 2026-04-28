2d55a33d0801fc4b17e77f37e5fbed56ff3c0d0f
wissalaitihya <wissalaitihya@gmail.com>
2026-04-28 12:48:44 +0100

    add Category, Task and User models

--- database/migrations/2026_04_27_133936_create_tasks_table.php
+++ database/migrations/2026_04_27_133936_create_tasks_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['to_do', 'in_progress', 'done'])->default('to_do');
+            $table->date('due_date')->nullable();
             $table->timestamps();
+            
         });
     }
 
